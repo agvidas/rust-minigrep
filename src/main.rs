c3d0e1468f05a825a516374d35bfb3b5a49d4a68
Gvidas <gvidas.abakanavicius@gmail.com>
2023-03-06 17:09:54 +0100

    Add parse config function
    Implement config instance. Use clone for now as performance does not matter at this stage

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,27 @@ use std::env;
 use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args);
 
-    let query = &args[1];
-    let filename = &args[2];
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
-
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
 
     println!("With the text:\n{}", contents);
+}
+
+struct Config {
+    query :String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { query, filename }
+    }
 }
