d2bc6c16404758fb4e26ab9f600bb70894056805
Gvidas <gvidas.abakanavicius@gmail.com>
2023-03-07 18:37:08 +0100

    Add search function and unit test

---
+++ README.md
@@ -0,0 +1,9 @@
+# An example of simple command line tool
+
+### To search in file by query:
+
+`cargo run <query> <path to file>`
+
+### Run tests:
+
+`cargo test`
--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,6 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
     if let Err(e) = rust_minigrep::run(config) {
         print!("Application error: {}", e);
         process::exit(1);
