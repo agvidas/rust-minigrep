d635bc890e2e16562856a93f58c25c55adece979
Gvidas <gvidas.abakanavicius@gmail.com>
2023-03-10 23:14:39 +0100

    Print errors to standard error

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else( |err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = rust_minigrep::run(config) {
-        print!("Application error: {}", e);
+        eprint!("Application error: {}", e);
         process::exit(1);
     }
 }
